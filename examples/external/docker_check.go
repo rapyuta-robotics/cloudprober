8c577f0a56352467c88143d70c2f99ae7423053b
Dhananjay Sathe <dhananjaysathe@gmail.com>
2018-06-21 23:17:56 +0530

    Improve docker stats and enure it runs on backing stores with devicemaopper thin pools

--- examples/external/docker_check.go
+++ examples/external/docker_check.go
@@ -10,32 +10,79 @@ import (
 	"log"
 	"github.com/golang/protobuf/proto"
 	epb "github.com/google/cloudprober/probes/external/proto"
+	"strconv"
 )
 
 var isserver = flag.Bool("server", false, "Whether to run in server mode")
+
+const (
+	BYTE     = 1.0 << (10 * iota)
+	KILOBYTE
+	MEGABYTE
+	GIGABYTE
+	TERABYTE
+)
+
+var interestingKeys = map[string]string{
+	"Pool Blocksize":               "pool_blocksize",
+	"Base DeviceSize":              "base_device_size",
+	"Data Space Used":              "data_space_used",
+	"Data Space Total":             "data_space_total",
+	"Data Space Available":         "data_space_available",
+	"Metadata Space Used":          "metadata_space_used",
+	"Metadata Space Total":         "metadata_space_total",
+	"Metadata Space Available":     "metadata_space_available",
+	"Thin Pool Minimum Free Space": "thin_pool_minimum_free_space",
+}
 
 func dockerProbe() (string, error) {
 	var payload []string
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	info, err := cli.Info(context.Background())
 	if err != nil {
-		return "",err
+		return "", err
 	}
-	diskUsage, err := cli.DiskUsage(context.Background())
-	if err != nil {
-		return "",err
+	for _, keyValTup := range info.DriverStatus {
+		promKey, ok := interestingKeys[keyValTup[0]]
+		if ok {
+			value, err := convertToBytes(keyValTup[1])
+			if err == nil {
+				payload = append(payload, fmt.Sprintf("%s %d", promKey, value))
+			}
+		}
 	}
-	payload = append(payload, fmt.Sprintf("docker_layers_total_size %d", diskUsage.LayersSize))
-	payload = append(payload, fmt.Sprintf("docker_images_count %d", len(diskUsage.Images)))
-	payload = append(payload, fmt.Sprintf("docker_container_total_count %d", info.Containers))
-	payload = append(payload, fmt.Sprintf("docker_containers_running_count %d", info.ContainersRunning))
+	payload = append(payload, fmt.Sprintf("container_total %d", info.Containers))
+	payload = append(payload, fmt.Sprintf("containers_running %d", info.ContainersRunning))
+	payload = append(payload, fmt.Sprintf("images %d", info.Images))
 	//payload = append(payload, fmt.Sprintf("success %t", true))
 
-	return fmt.Sprintf(strings.Join(payload, "\n")),nil
+	return fmt.Sprintf(strings.Join(payload, "\n")), nil
+}
+
+func convertToBytes(input string) (int64, error) {
+	frags := strings.Split(input, " ")
+	valStr, unit := frags[0], strings.ToLower(frags[1])
+	val, err := strconv.ParseFloat(valStr, 64)
+	if err != nil {
+		return 0, err
+	}
+	multiplier := 1
+	switch {
+	case unit == "kb":
+		multiplier = KILOBYTE
+	case unit == "mb":
+		multiplier = MEGABYTE
+	case unit == "gb":
+		multiplier = GIGABYTE
+	case unit == "tb":
+		multiplier = TERABYTE
+	}
+	return int64(val * float64(multiplier)), nil
+
 }
 
 func main() {
